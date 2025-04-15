eab1084a565e4641b92d2bae0ebaedcac4f215f5
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2025-04-15 19:59:17 +0530

    feat: implement Profile page with user details and property listings

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -5,15 +5,28 @@ import connectDB from "@/config/db";
 import Property from "@/models/Property";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
+import { convertToSerializableObject } from "@/utils/convertToObject";
 
 async function PropertyDetailsPage({ params }) {
   await connectDB();
-  const property = await Property.findById(params.id).lean();
+  const propertyDoc = await Property.findById(params.id).lean();
+  const property = convertToSerializableObject(propertyDoc);
+
+  if(!property) {
+    return (
+      <div className="container m-auto py-24 px-6">
+        <h1 className="text-3xl font-bold mb-4">Property not found</h1>
+        <Link href="/properties" className="text-blue-500 hover:text-blue-600">
+          Back to Properties
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <>
       <PropertyHeaderImg image={property.images[0]} />
-
-      {/* Go Back */}
+      {/* Go Back to properties page button */}
       <section>
         <div className="container m-auto py-6 px-6">
           <Link
