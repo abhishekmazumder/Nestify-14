af70ff1942a2ede20e30be4eaa28db91e42d57f8
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2025-04-21 15:42:05 +0530

    feat: implement addMessage function for sending user messages with validation

---
+++ app/actions/addMessage.js
@@ -0,0 +1,41 @@
+"use server";
+
+import connectDB from "@/config/db";
+import Message from "@/models/Message";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+async function addMessage(formData) {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+
+  const { userId } = sessionUser;
+  const recipientId = formData.get("recipient");
+
+  if (userId === recipientId) {
+    throw new Error("You cannot send a message to yourself!");
+  }
+
+  const messageData = {
+    sender: userId,
+    recipient: recipientId,
+    property: formData.get("property"),
+    name: formData.get("name"),
+    email: formData.get("email"),
+    phone: formData.get("phone"),
+    body: formData.get("body"),
+  };
+
+  const newMessage = await Message.create(messageData);
+
+  if (!newMessage) {
+    throw new Error("Failed to create message");
+  }
+  return { submitted: true };
+}
+
+export default addMessage;
